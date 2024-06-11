d5f5ebfc4d93b145c8e97a901cad72de52d9d153
reena-10 <reenamahto310@gmail.com>
2024-06-11 20:31:44 +0530

    Some changes done

---
+++ HasARelationExample/src/Address.java
@@ -0,0 +1,17 @@
+class Address {
+	String city,state,country;
+	public Address(String city, String state, String country) {
+		this.city=city;
+		this.state=state;
+		this.country=country;
+		
+	}
+}
+public class Employee {
+	public static void main(String [] args)
+	int id;
+	String name;
+	Address address;
+}
+	
+}
---
+++ ReenaApplication/src/program.java
@@ -0,0 +1,30 @@
+
+public class program {
+
+	public static void main(String[] args) {
+	 int day;
+	 day=6;
+	
+		switch(day) {
+		 case 1  :
+			 System.out.println("monday");
+			break ; 
+		 case 2  :
+			 System.out.println("tuesday");
+			 break ;
+		 case 3  :
+			 System.out.println("Wednesday");
+			 break ;
+		 case 4  :
+			 System.out.println("Thrusday");
+			 break ;
+		 case 5  :
+			 System.out.println("Friday");
+			 break ;
+		 default :
+			 System.out.println("not a valid day ");
+			 
+	}
+
+}
+}
