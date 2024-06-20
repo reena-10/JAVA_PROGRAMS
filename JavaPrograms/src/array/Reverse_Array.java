4eeaaa194d34dbf6fb51c5e5d838a18eaf9d7cc8
reena-10 <reenamahto310@gmail.com>
2024-06-20 15:33:07 +0530

    June 20

---
+++ JavaPrograms/src/array/Reverse_Array.java
@@ -0,0 +1,13 @@
+package array;
+
+public class Reverse_Array {
+
+	public static void main(String[] args) {
+     int arr[]= {1,2,3,4,5};
+     for (int i=5-1;i>=0;i--) {
+    	 System.out.print(arr[i]+" ");
+     }
+     System.out.println();
+	}
+
+}
