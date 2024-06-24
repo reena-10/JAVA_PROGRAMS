8836982286b6bec961c7cff1d37d2deef2a1a253
reena-10 <reenamahto310@gmail.com>
2024-06-24 13:03:39 +0530

    June 18

---
+++ JavaPrograms/src/static_Keyword/StaticBlock_Example.java
@@ -0,0 +1,11 @@
+package static_Keyword;
+
+public class StaticBlock_Example {
+ static {
+	 System.out.println("Static Block is invoked");
+ }
+	public static void main(String[] args) {
+      System.out.println("Hello Java");
+	}
+
+}
