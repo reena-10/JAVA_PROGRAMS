a3b9da8a0eb473c63f563827a22e10410e0fea4f
reena-10 <reenamahto310@gmail.com>
2024-07-12 17:43:04 +0530

    July 12

---
+++ JavaPrograms/src/arrayList/Iterate_Example.java
@@ -0,0 +1,20 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Iterate_Example {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        // Get the iterator
+        Iterator<String> it = cars.iterator();
+
+        // Print the first item
+        System.out.println(it.next());
+    }
+}
