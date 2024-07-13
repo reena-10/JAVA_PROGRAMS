0600c9754adcdb467a353ddbc745016ace356320
reena-10 <reenamahto310@gmail.com>
2024-07-13 21:59:28 +0530

    July 13

---
+++ JavaPrograms/src/string/String_Concatenation.java
@@ -0,0 +1,15 @@
+package string;
+
+public class String_Concatenation {
+	    public static void main(String[] args) {
+	        String str1 = "Hello, ";
+	        String str2 = "world!";
+
+	        // Concatenate the strings
+	        String result = str1 + str2;
+
+	        System.out.println("Concatenated string: " + result);
+	    }
+	}
+
+
---
+++ JavaPrograms/src/string/String_ToUppercase.java
@@ -0,0 +1,10 @@
+package string;
+
+public class String_ToUppercase {
+  public static void main (String[] args) {
+	  String s ="Hello World";
+	  String us = s.toUpperCase();
+	  System.out.println("Original String: "+s);
+	  System.out.println("Uppercase String: "+us);
+  }
+}
