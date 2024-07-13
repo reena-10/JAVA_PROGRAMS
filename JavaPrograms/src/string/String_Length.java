7d0efefddd66856b7a1bd57cb46a6a462643980f
reena-10 <reenamahto310@gmail.com>
2024-07-13 22:20:09 +0530

    July 13

---
+++ JavaPrograms/src/string/String_Length.java
@@ -0,0 +1,10 @@
+package string;
+
+public class String_Length {
+ public static void main(String []args) {
+	 String myString ="Hello,Java!";
+	 int stringLength = myString.length();
+	 
+	 System.out.println(" The length of the String '"+ myString+"'is "+stringLength);
+ }
+}
