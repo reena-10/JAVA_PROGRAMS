78424e192d55195aec76556d29e5dcd6e337500e
reena-10 <reenamahto310@gmail.com>
2024-07-15 20:41:14 +0530

    July 15

---
+++ JavaPrograms/src/map/Frequency.java
@@ -0,0 +1,25 @@
+package map;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Frequency {
+
+	public static void main(String[] args) {
+	String s="Hellomoon";
+	char[] arr = s.toCharArray();
+	HashMap <Character,Integer> map = new HashMap<>();
+	
+	for (char c : arr) {
+		if (map.containsKey(c)) {
+			map.put(c,map.get(c)+1);
+		}
+		else {
+			map.put(c, 1);
+		}
+	}
+	for (Map.Entry<Character,Integer> Pair : map.entrySet()) {
+		System.out.println(Pair.getKey()+"--->"+Pair.getValue());
+	}
+   	}
+
+}
