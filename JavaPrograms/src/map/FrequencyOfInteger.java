8fe7988566ec7ec47f615a43f2eebd2ce1e5e472
reena-10 <reenamahto310@gmail.com>
2024-07-16 19:04:56 +0530

    July 16

---
+++ JavaPrograms/src/map/FrequencyOfInteger.java
@@ -0,0 +1,34 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class FrequencyOfInteger {
+
+	public static void main(String[] args) {
+		int arr[] = {1,2,4,6,8,9,4,2,2,46};
+
+	HashMap<Integer,Integer> map = new HashMap<>();
+				
+	for(int c : arr) {
+	  if(map.containsKey(c)) {
+		   map.put(c, map.get(c)+1) ;
+					   
+	    }
+		else {
+					   
+			 map.put(c, 1) ;
+		}
+					
+	}
+				
+ for( Entry<Integer, Integer> Frequency : map.entrySet() ) {
+		    	  
+	System.out.println(Frequency.getKey()+"----->"+Frequency.getValue());
+  }
+				
+				
+ }
+}
+
+
---
+++ JavaPrograms/src/map/Linked_HashMap.java
@@ -0,0 +1,31 @@
+package map;
+
+
+		import java.util.HashMap;
+		import java.util.LinkedHashMap;
+		import java.util.Map;
+
+		public class Linked_HashMap {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+
+				
+				LinkedHashMap<String, String> country = new LinkedHashMap<>();
+				country.put(" Bihar ", "  patna");
+				country.put(" france ", " paris ");
+				country.put(" Japan ", " tokyo ");
+				
+				
+				
+				for(Map.Entry<String, String>m : country.entrySet()) {
+					
+					System.out.println(m);
+				}
+				
+			}
+
+		}
+	
+
+
