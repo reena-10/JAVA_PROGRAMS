a084d6877c0a5265d2cdb612e10357b12db58265
reena-10 <reenamahto310@gmail.com>
2024-07-21 22:28:46 +0530

    July 21

---
+++ JavaPrograms/src/stack/Pop_Method.java
@@ -0,0 +1,33 @@
+package stack;
+import java.util.Iterator;
+import java.util.Stack;
+public class Pop_Method {
+
+	public static void main(String[] args) {
+    Stack<String> stack = new Stack<>();
+    
+    // Add elements to Stack
+    stack.push("Reena");
+    stack.push("Meena");
+    stack.push("Teena");
+    stack.push("Kareena");
+    stack.push("Katrina");
+	
+	System.out.println("Before pop method");
+	Iterator<String> i= stack.iterator();
+    while (i.hasNext()) {
+        System.out.println(i.next());
+    
+    }  
+    System.out.println();
+  //Remove element stacks
+	String element = stack.pop();
+	
+	System.out.println("After pop method");
+	   Iterator<String> itr= stack.iterator();
+       while (itr.hasNext()) {
+           System.out.println(itr.next());
+       }
+    
+	}
+}
