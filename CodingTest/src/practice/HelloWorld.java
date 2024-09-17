3d723c06f6d88e0910730332e182d5dc725b94e6
reena-10 <reenamahto310@gmail.com>
2024-09-17 12:08:00 +0530

    Sep 17

---
+++ CodingTest/src/practice/HelloWorld.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello world\nReena Mahto");
+	
+    int c = -5+8*6;
+    System.out.println(c);
+}
+}
