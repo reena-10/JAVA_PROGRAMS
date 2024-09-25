d2d41d709fe3d553aaea1128178c1df07a710b51
reena-10 <reenamahto310@gmail.com>
2024-09-25 18:55:14 +0530

    Sep 25

--- CodingTest/src/practice/PrimeNumbers.java
+++ CodingTest/src/practice/PrimeNumbers.java
@@ -1,10 +1,32 @@
 package practice;
 
 public class PrimeNumbers {
-
+ 
+	 static void prime_N(int N) {
+		 int x,y,flg;
+		 System.out.println("All the prime within 1 and "+N+ " are:");
+		 for (x=1;x<=N; x++) {
+			 if(x==1||x==0)
+				 continue;
+			 flg =1;
+			 for(y=2;y<=x/2;++y) {
+				if(x==1||x==0)
+					continue;
+				flg=1;
+				for(y=2;y<=x/2; ++y) {
+					if(x%y==0) {
+						flg=0;
+						break;
+					}
+				}
+				if(flg == 1)
+					System.out.println(x+" ");
+			 }
+		 }
+	 }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+     int N=45;
+     prime_N(N);
 	}
 
 }
