36c993c91f5de16d50da377a18e5711c5828f508
reena-10 <reenamahto310@gmail.com>
2024-09-26 21:44:18 +0530

    Sep 26

---
+++ CodingTest/src/practice/Armstrong_Number.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Armstrong_Number {
+ static void ArmStrongNum(int l,int h) {
+	 for(int j =l+1;j<h;j++) {
+		//Pending 
+	 }
+ }
+	public static void main(String[] args) {
+    //Pending
+	}
+
+}
