591bacced4537071b88b9b624e475facedd047a6
reena-10 <reenamahto310@gmail.com>
2024-12-30 23:22:23 +0530

    Dec 30

---
+++ CodingTest/src/practice/ASCII_Value.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class ASCII_Value {
+
+	    public static void main(String[] args) {
+
+	        char ch = 'a';
+	        int ascii = ch;
+	        // You can also cast char to int
+	        int castAscii = (int) ch;
+
+	        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+	        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+	    }
+	
+
+}
