988b96130dae4575aef522a74ca0c76c11f849a0
reena-10 <reenamahto310@gmail.com>
2024-12-30 22:50:11 +0530

    Dec 30

---
+++ CodingTest/src/practice/Practice2.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+    int i = sc.nextInt();
+    double j = sc.nextDouble();
+    
+    System.out.println("String:"+" "+s);
+    System.out.println("Double:"+" "+j);
+    System.out.println("Integer:"+" "+i);
+	}
+
+}
