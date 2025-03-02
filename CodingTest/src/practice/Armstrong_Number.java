7ba1c9ef4ffef80f4500b89ac8e54ba625b5937e
reena-10 <reenamahto310@gmail.com>
2025-03-02 12:25:58 +0530

    March 2

--- CodingTest/src/practice/Armstrong_Number.java
+++ CodingTest/src/practice/Armstrong_Number.java
@@ -1,13 +1,35 @@
 package practice;
 
-public class Armstrong_Number {
- static void ArmStrongNum(int l,int h) {
-	 for(int j =l+1;j<h;j++) {
-		//Pending 
-	 }
- }
-	public static void main(String[] args) {
-    //Pending
-	}
+import java.util.Scanner;
 
-}
+public class Armstrong_Number {
+	
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        
+	        System.out.print("Enter a number: ");
+	        int number = scanner.nextInt();
+	        
+	        if (isArmstrong(number)) {
+	            System.out.println(number + " is an Armstrong number.");
+	        } else {
+	            System.out.println(number + " is not an Armstrong number.");
+	        }
+	        
+	        scanner.close();
+	    }
+
+	    public static boolean isArmstrong(int number) {
+	        int originalNumber = number;
+	        int sum = 0;
+	        int numberOfDigits = String.valueOf(number).length();
+	        
+	        while (number > 0) {
+	            int digit = number % 10;
+	            sum += Math.pow(digit, numberOfDigits);
+	            number /= 10;
+	        }
+	        
+	        return sum == originalNumber;
+	    }
+	}
